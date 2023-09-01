f362e5d13267bb7fbbf83b0727604b01fa0a81bb
wancheullim <gop78@gsitm.com>
2023-09-01 11:14:37 +0900

    bootstrap
    - bootstrap 기본파일 적용 및 샘플 페이지

---
+++ src/main/java/com/example/pokeapi/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.pokeapi.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "sample";
+    }
+}
