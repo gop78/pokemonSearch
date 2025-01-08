224305b7c90122c034689f3f902903500def6e22
임완철 <gop78@gsitm.com>
2025-01-09 08:56:09 +0900

    포켓몬 타입 아이콘 추가하는 함수 리팩토링

--- src/main/resources/static/js/pokemon/pokemon.js
+++ src/main/resources/static/js/pokemon/pokemon.js
@@ -7,21 +7,22 @@ $(() => {
  * 포켓몬 타입 아이콘 추가
  */
 function setTypeIcon() {
-   let pokemonList = $('.row.gutters').children('.pokemonCard');
-   pokemonList.each((i, v) => {
-      let innerHtml = '';
-      let iconWrapp = $(v).find('.iconWrapp');
+   const pokemonList = $('.row.gutters').children('.pokemonCard');
 
-      let types = $(v).find('.types');
-      types.each((i2, v2) => {
-         let typeCount = types.length;
-         let type = $(v2).val();
-         let iconClass = i2 === 0 && typeCount > 1 ? 'icon first ' + type : 'icon ' + type;
-         innerHtml += `
+   pokemonList.each((_, pokemonCard) => {
+      const iconWrapp = $(pokemonCard).find('.iconWrapp');
+      const types = $(pokemonCard).find('.types');
+
+      const innerHtml = types.map((index, typeElement) => {
+         const type = $(typeElement).val();
+         const typeCount = types.length;
+         const iconClass = index === 0 && typeCount > 1 ? `icon first ${type}` : `icon ${type}`;
+         return `
             <div class="${iconClass}" title="${type}">
                 <img src="/svg/${type}.svg">
-            </div>`
-      });
+            </div>`;
+      }).get().join('');
+
       iconWrapp.append(innerHtml);
    });
 }
